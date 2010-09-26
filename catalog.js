2e190c139f66a96f991b6c67a07b24d867fd6bdc
Eric di Domenico <eric@didomenico.com>
2010-09-26 08:56:02 -0700

    Add some more tests

--- catalog.js
+++ catalog.js
@@ -1,1 +1,24 @@
+function Category(xml) {
+   var category = $(xml).first();
+   this.name = $(xml).children("name:first").text();
+   this.id = $(xml).attr("id");
+   this.parentId = $(xml).children("parent:first").text();
+}
 
+function parseCategories(xml) {
+   var categories = [];
+   var index = 0;
+   $(xml).find("category").each(function() {
+      categories[index++] = new Category($(this));
+   });
+   return categories;
+}
+
+function parseItemIds(xml) {
+   var itemIds = [];
+   var index = 0;
+   $(xml).find("item-id").each(function() {
+      itemIds[index++] = $(this).text();
+   });
+   return itemIds;
+}
