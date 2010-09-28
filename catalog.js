b103e388c8fae1f82c1a4524f6b571ae4e8db368
Eric di Domenico <eric@didomenico.com>
2010-09-27 20:56:12 -0700

    add Item

--- catalog.js
+++ catalog.js
@@ -1,8 +1,19 @@
 function Category(xml) {
-   var category = $(xml).first();
    this.name = $(xml).children("name:first").text();
    this.id = $(xml).attr("id");
    this.parentId = $(xml).children("parent:first").text();
+}
+
+function Item(xml) {
+   this.id = $(xml).attr("id");
+   this.type = $(xml).attr("type");
+
+   var product = $(xml).children("product-info:first");
+   this.name = $(product).children("name:first").text();
+
+   var thumbnails = $(product).children("thumbnails")
+   this.smallThumbnail = $(thumbnails).children("thumbnail[label=small]").text();
+   this.largeThumbnail = $(thumbnails).children("thumbnail[label=large]").text();
 }
 
 function parseCategories(xml) {
